9dbb2f936f9f97ae01a5f5436282550d94a7bea8
sonchangwoo <thsckddn97@naver.com>
2024-09-26 15:11:57 +0900

    create Field & Constructor & getter & setter

--- src/ATM/User.java
+++ src/ATM/User.java
@@ -1,5 +1,37 @@
 package ATM;
 
 public class User {
-
+	
+	private int code;
+	private String name;
+	private String phone;
+	
+	public User() {}
+	
+	public User(int code, String name,String phone) {
+		this.code = code;
+		this.name = name;
+		this.phone = phone;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public User clone() {
+		return new User(this.code,this.name,this.phone);
+	}
+	
 }
