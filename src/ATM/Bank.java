edb7a23c1a9c8c282a371facb9eb9a4b1b6e4aec
sonchangwoo <thsckddn97@naver.com>
2024-09-26 16:36:43 +0900

    runMenu & inputNumber 메서드

--- src/ATM/Bank.java
+++ src/ATM/Bank.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Bank {
 	
+	private final int USER_JOIN = 1;
+	private final int USER_LEAVE = 2;
+	private final int DEPOSIT = 3;
+	private final int TRANSFER = 4;
+	private final int ACCOUNT_OPEN = 5;
+	private final int ACCOUNT_WITHDRAW = 6;
+	private final int ACCOUNT_SEARCH = 7;
+	private final int EXIT = 0;
+
 	private final String BRAND;
 	private boolean isRun;
 	
@@ -44,6 +53,32 @@ public class Bank {
 		System.out.println("6.계좌 철회");
 		System.out.println("7.계좌 조회");
 		System.out.println("0.종료");
+	}
+	
+	private int inputNumber(String message) {
+		int number = -1;
+		try {
+			System.out.println(message);
+			String input = scan.next();
+			number = Integer.parseInt(input);
+		}catch(Exception e) {
+			System.out.println("숫자만 입력하세요.");
+			e.printStackTrace();
+		}
+		return number;
+	}
+	
+	
+	
+	private void runMenu(int sel) {
+		if(sel == USER_JOIN) {}
+		else if(sel == USER_LEAVE) {}
+		else if(sel == DEPOSIT) {}
+		else if(sel == TRANSFER) {}
+		else if(sel == ACCOUNT_OPEN) {}
+		else if(sel == ACCOUNT_WITHDRAW) {}
+		else if(sel == ACCOUNT_SEARCH) {}
+		else if(sel == EXIT) {}
 	}
 	
 }
