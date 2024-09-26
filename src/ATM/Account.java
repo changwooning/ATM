277f207b57e6ee28d858b4808ed87394ca1cdaaa
sonchangwoo <thsckddn97@naver.com>
2024-09-26 15:48:40 +0900

    Constructor & getter & setter & clone 메서

--- src/ATM/Account.java
+++ src/ATM/Account.java
@@ -3,8 +3,47 @@ package ATM;
 public class Account {
 	
 	private int userCode;
-	private String number;
+	private String accountNumber;
 	private String password;
 	private int balance;
+	
+	public Account() {}
+	
+	public Account(int userCode,String accountNumber,String password) {
+		this.userCode = userCode;
+		this.accountNumber = accountNumber;
+		this.password = password;
+	}
+	
+	public Account(int userCode,String accountNumber,String password,int balance) {
+		this.userCode = userCode;
+		this.accountNumber = accountNumber;
+		this.password = password;
+		this.balance = balance;
+	}
+	
+	public int getUserCode() {
+		return userCode;
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public boolean equalsPassword() {
+		return this.password.equals(password);
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public Account clone() {
+		return new Account(userCode,accountNumber,password,balance);
+	}
 
 }
